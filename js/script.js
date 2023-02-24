f28132e16a6b95ccb5e026abba517afe27949f50
CrisMatias <95983798+CrisMatias@users.noreply.github.com>
2023-02-24 19:33:21 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,101 +1,3 @@
-// function pressionar1(){
-//     let contenido1 = document.getElementById('datos1')
-//     let contenido2 = document.getElementById('datos2')
-//     let contenido3 = document.getElementById('datos3')
-//     let contenido4 = document.getElementById('datos4')
-//     let contenido5 = document.getElementById('datos5')
-//     let contenido6 = document.getElementById('datos6')
-//     if(contenido1.classList.contains('container-text')){
-//         contenido1.classList.remove("container-text")
-//         contenido2.classList.add("container-text")
-//         contenido3.classList.add('container-text')
-//         contenido4.classList.add('container-text')
-//         contenido5.classList.add('container-text')
-//         contenido6.classList.add('container-text')
-//     }
-// }
-// function pressionar2(){
-//     let contenido1 = document.getElementById('datos1')
-//     let contenido2 = document.getElementById('datos2')
-//     let contenido3 = document.getElementById('datos3')
-//     let contenido4 = document.getElementById('datos4')
-//     let contenido5 = document.getElementById('datos5')
-//     let contenido6 = document.getElementById('datos6')
-//     if(contenido2.classList.contains('container-text')){
-//         contenido2.classList.remove("container-text")
-//         contenido1.classList.add("container-text")
-//         contenido3.classList.add('container-text')
-//         contenido3.classList.add('container-text')
-//         contenido4.classList.add('container-text')
-//         contenido5.classList.add('container-text')
-//         contenido6.classList.add('container-text')
-//     }
-// }
-
-// function pressionar3(){
-//     let contenido1 = document.getElementById('datos1')
-//     let contenido2 = document.getElementById('datos2')
-//     let contenido3 = document.getElementById('datos3')
-//     let contenido4 = document.getElementById('datos4')
-//     let contenido5 = document.getElementById('datos5')
-//     let contenido6 = document.getElementById('datos6')
-//     if(contenido3.classList.contains('container-text')){
-//         contenido3.classList.remove("container-text")
-//         contenido1.classList.add("container-text")
-//         contenido2.classList.add("container-text")
-//         contenido4.classList.add('container-text')
-//         contenido5.classList.add('container-text')
-//         contenido6.classList.add('container-text')
-//     }
-// }
-// function pressionar4(){
-//     let contenido1 = document.getElementById('datos1')
-//     let contenido2 = document.getElementById('datos2')
-//     let contenido3 = document.getElementById('datos3')
-//     let contenido4 = document.getElementById('datos4')
-//     let contenido5 = document.getElementById('datos5')
-//     let contenido6 = document.getElementById('datos6')
-//     if(contenido4.classList.contains('container-text')){
-//         contenido4.classList.remove("container-text")
-//         contenido1.classList.add("container-text")
-//         contenido2.classList.add("container-text")
-//         contenido3.classList.add('container-text')
-//         contenido5.classList.add('container-text')
-//         contenido6.classList.add('container-text')
-//     }
-// }
-// function pressionar5(){
-//     let contenido1 = document.getElementById('datos1')
-//     let contenido2 = document.getElementById('datos2')
-//     let contenido3 = document.getElementById('datos3')
-//     let contenido4 = document.getElementById('datos4')
-//     let contenido5 = document.getElementById('datos5')
-//     let contenido6 = document.getElementById('datos6')
-//     if(contenido5.classList.contains('container-text')){
-//         contenido5.classList.remove("container-text")
-//         contenido1.classList.add("container-text")
-//         contenido2.classList.add("container-text")
-//         contenido3.classList.add("container-text")
-//         contenido4.classList.add('container-text')
-//         contenido6.classList.add('container-text')
-//     }
-// }
-// function pressionar6(){
-//     let contenido1 = document.getElementById('datos1')
-//     let contenido2 = document.getElementById('datos2')
-//     let contenido3 = document.getElementById('datos3')
-//     let contenido4 = document.getElementById('datos4')
-//     let contenido5 = document.getElementById('datos5')
-//     let contenido6 = document.getElementById('datos6')
-//     if(contenido6.classList.contains('container-text')){
-//         contenido6.classList.remove("container-text")
-//         contenido1.classList.add("container-text")
-//         contenido2.classList.add("container-text")
-//         contenido3.classList.add("container-text")
-//         contenido4.classList.add('container-text')
-//         contenido5.classList.add('container-text')
-//     }
-// }
 $('.botnData').click(function() { 
     let listaBotones = $('.botnData');
     let listaDiv = $('.container-info')
@@ -119,14 +21,10 @@ $('.row-active').click(function() {
     let listaHidden = $('.row-hidden')
     for (let i = 0; i < listaRow.length ; i++){
         if(listaRow[i].id == $(this).attr('id') ){
-            $(listaHidden[i]).toggle('slow')
+            $(listaHidden[i]).toggleClass('slow')
             colores = listaRow[i]
-            $(colores).addClass('eduBtnActive');
+            $(colores).toogleClass('eduBtnActive');
 //          $(colores).css('background-color', '#4D4637')
-        }
-        else {
-          colores = listaRow[i];
-          $(colores).removeClass('eduBtnActive');
         }
     }
     
